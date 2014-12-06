2f4e3d18edfbdc39616331bb8d1eed7786ef73bb
Jesse Maw <jmaw001@ucr.edu>
2014-12-06 04:09:08 -0800

    added new script and updated rshell

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -11,189 +11,240 @@
 
 using namespace std;
 
+static void handler(int signum)
+{
+        signal(signum, SIG_IGN);
+}
 void execute(char* source[])
 {
-	int pid1;
-	pid1=fork();
-	if(pid1<0)
+	/*int j=0;
+	char** holy=used;
+	while(holy[j]!=NULL)
 	{
-		perror("fork");
-		exit(1);
-	}
-	if(pid1==0)
-	{
-		if(execvp(source[0], source)==-1)
+		strcat(holy[j],"/");
+		strcat(holy[j],source[0]);
+		cout << holy[j] << endl;
+		j++;
+	}*/
+        int pid1;
+        pid1=fork();
+        if(pid1<0)
+        {
+                perror("fork");
+                exit(1);
+        }
+        if(pid1==0)
+        {
+                signal(SIGINT, SIG_DFL);
+                /*for(int l=0;l<j;l++)
+		{
+			execv(holy[l], source);
+		}*/
+		if(execvp(source[0],source)==-1)
 			perror("execvp");
-		exit(1);
-	}
-	if(pid1>0)
-	{
-		if(wait(NULL)==-1)
-			perror("wait");
-	}		
+                exit(1);
+        }
+        if(pid1>0)
+        {
+                signal(SIGINT, handler);
+                if(wait(NULL)==-1)
+                        perror("wait");
+        }
 }
 void execute2(char* source[])
 {
-	int fd[2];
-	if(pipe(fd)==-1)
-		perror("pipes");
-	int pid1;
-	pid1=fork();
-	if(pid1<0)
-		perror("fork");
-	if(pid1==0)
-	{
-		if(-1==dup2(fd[1],1))
-			perror("dup2 problem");
-		//if(-1==close(fd[0]))
-			//perror("cant close stdin");
+        int fd[2];
+        if(pipe(fd)==-1)
+                perror("pipes");
+        int pid1;
+        pid1=fork();
+        if(pid1<0)
+                perror("fork");
+        if(pid1==0)
+        {
+                if(-1==dup2(fd[1],1))
+                        perror("dup2 problem");
+                //if(-1==close(fd[0]))
+                        //perror("cant close stdin");
 		if(execvp(source[0], source)==-1)
-			perror("execvp");
-		exit(1);
-	}
-	if(pid1>0)
-	{
-		if(-1==dup2(fd[0],0))
-			perror("cannot dup stdin");
-		if(-1==close(fd[1]))
-			perror("cant close stdout");
-		if(wait(NULL)==-1)
-			perror("wait");
-	}	
+                        perror("execvp");
+                exit(1);
+        }
+        if(pid1>0)
+        {
+                if(-1==dup2(fd[0],0))
+                        perror("cannot dup stdin");
+                if(-1==close(fd[1]))
+                        perror("cant close stdout");
+                if(wait(NULL)==-1)
+                        perror("wait");
+        }
 }
+/*void makearray(char** easy)
+{
+	char* pPath= getenv("PATH");
+	char* token;
+	token=strtok(pPath, ":");
+	easy[0]=token;
+	int j=1;
+	token=strtok(NULL, ":");
+	while(token!=NULL)
+	{
+		easy[j]=token;
+		j++;
+		token=strtok(NULL, ":");
+	}
+	easy[j]=NULL;
+}*/
 int  main()
 {
-	cout << "Welcome to Jmaw001's rshell program! Please type in any bash commands!" << endl;
-	string exite="exit";
-	int original=0;
-	vector <char*> lines(0);
-	char cstring[1024];			//getline cstring
-	char * pch;				//cstring for word parsing
-	char *scapegoat;			//cstring for comment parsing
-	int i=0;				//initializing variables 
-	int lettercount=0;
-	char * cmdlist[500];			//declare argv for execvp		
-	char name[8];
-	bool has=false;
-	int pid;
-	while(1)				//infinite while loop for command shell till
-	{					//user inputs exit
-		for(int k=0;k<500;k++)
-		{
-			cmdlist[k]=NULL;		//flushes argv with NULL so we can take in more cmds
-		}
-		if(getlogin()==NULL)		//just error messages for login and host
-			perror("The login is unretreivable therefore NULL");
-		if(gethostname(name, 8)==-1)
-			perror("Error in hostname retreival!");
-		cout << getlogin()  << "@" << name << "$ ";	//extra credit!
-		cin.getline(cstring, 1024);			//takes input
-		scapegoat=strtok(cstring, "#");			//removes comments
-		pch = strtok(scapegoat, " &;");			//starts parsing into single words
-		if(strcmp(pch,exite.c_str())==0)		//my exit function
-		{
-			cout << "Good Bye!" << endl;
-			exit(1);
-		}
-          	while(pch[lettercount]!='\0'&&pch[lettercount]!=';'&&pch[lettercount]!='&')
-            	{
-               		lettercount++;				//gets word length to get
-             		original++;
-		}
-		cmdlist[i]=pch;						//rid of the straggling connectors
-            	i++;
-		lettercount=0;
-		while(pch!=NULL)        //inner while loop parse rest of commands in the line.
-               	{
-                     	pch = strtok(NULL, " ");
-			if(pch==NULL)
-			{
-				execute(cmdlist);
-				i=0;
-			}
-			else
-			{
-				if(strcmp(pch, "|")!=0)
-				{
-					while(pch[lettercount]!='\0'&&pch[lettercount]!=';'
-					&&pch[lettercount]!='&')
-                        		{
-                               		 	lettercount++;
-                        		}
-                        		cmdlist[i]=pch;
-					i++;
-                        		lettercount=0;
-				}
-				else
-				{
-					has=true;
-					break;
-				}
-			}
-		}
-		if(has)
-		{
-			int j=0;
-			pch= strtok(NULL, "|");
-			while(pch!='\0')
-			{
-				lines.push_back(pch);
-				pch=strtok(NULL, "|");
-			}
-			int fd[2];
-			if(pipe(fd)==-1)
-				perror("Error in pipe");
-			pid=fork();
-			if(pid<0)
-				perror("fork error");
-			else if(pid==0)
-			{
-				if(dup2(fd[1],1)==-1)
-					perror("dup2");
-				if(-1==close(fd[0]))
-					perror("close");
-				if(-1==execvp(cmdlist[0], cmdlist))
-					perror("execvp");
-				exit(1);
-			}
-			else
-			{
-				int stdinput=dup(0);
-				if(stdinput==-1)
-					perror("dup");
-				if(dup2(fd[0],0)==-1)
-					perror("dup2");
-				if(close(fd[1]))
-					perror("close");
-				if(wait(NULL)==-1)
-					perror("wait");
-				while(lines.size()!=0)
-				{
-					for(int i=0; i<500;i++)
-						cmdlist[i]=NULL;
-					j=0;
-					pch=strtok(lines.at(0), " ");
-					cmdlist[j]=pch;
-					while(pch!=NULL)
-					{
-						pch=strtok(NULL, " ");
-						j++;
-						cmdlist[j]=pch;
-					}
-					lines.erase(lines.begin());
-					if(lines.size()==0)
-					{
-						execute(cmdlist);
-					}
-					else
-					{
-						execute2(cmdlist);
-					}
-				}
-				if(dup2(stdinput,0)==-1)
-					perror("stdin restore failed");
-			}
-		}
-	}
-	return 0;
+        cout << "Welcome to Jmaw001's rshell program! Please type in any bash commands!" << endl;
+        string exite="exit";
+        int original=0;
+        vector <char*> lines(0);
+        char cstring[1024];                     //getline cstring
+        char * pch;                             //cstring for word parsing
+        char *scapegoat;                        //cstring for comment parsing
+        int i=0;                                //initializing variables 
+        int lettercount=0;
+        char * cmdlist[500];                    //declare argv for execvp               
+        char name[8];
+        bool has=false;
+        int pid;
+        char pw[BUFSIZ];
+        signal(SIGINT, handler);
+	//char* hou[256];
+	//makearray(hou);
+        while(1)                                //infinite while loop for command shell till
+        {                                       //user inputs exit
+                for(int k=0;k<500;k++)
+                {
+                        cmdlist[k]=NULL;                //flushes argv with NULL so we can take in more cmds
+                }
+                if(getcwd(pw, BUFSIZ)==NULL)
+                        perror("getcwd");
+                if(getlogin()==NULL)            //just error messages for login and host
+                        perror("The login is unretreivable therefore NULL");
+                if(gethostname(name, 8)==-1)
+                        perror("Error in hostname retreival!");
+                cout << getlogin()  << "@" << name << pw <<  "$ ";      //extra credit!
+                cin.getline(cstring, 1024);                     //takes input
+                scapegoat=strtok(cstring, "#");                 //removes comments
+                pch = strtok(scapegoat, " &;");                 //starts parsing into single words
+                if(strcmp(pch,exite.c_str())==0)                //my exit function
+                {
+                        cout << "Good Bye!" << endl;
+                        exit(1);
+                }
+                while(pch[lettercount]!='\0'&&pch[lettercount]!=';'&&pch[lettercount]!='&')
+                {
+                        lettercount++;                          //gets word length to get
+                        original++;
+                }
+                cmdlist[i]=pch;                                         //rid of the straggling connectors
+                i++;
+                lettercount=0;
+                while(pch!=NULL)        //inner while loop parse rest of commands in the line.
+                {
+                        pch = strtok(NULL, " ");
+                        if(pch==NULL)
+                        {
+                                if(strcmp(cmdlist[0],"cd")==0)
+                                {
+                                        char noob[]="./";
+                                        strcat(noob, cmdlist[1]);
+                                        chdir(noob);
+                                }
+                                else
+                                {	
+                                        execute(cmdlist);
+                                }
+                                i=0;
+                        }
+                        else
+                        {
+                                if(strcmp(pch, "|")!=0)
+                                {
+                                        while(pch[lettercount]!='\0'&&pch[lettercount]!=';'
+                                        &&pch[lettercount]!='&')
+                                        {
+                                                lettercount++;
+                                        }
+                                        cmdlist[i]=pch;
+                                        i++;
+                                        lettercount=0;
+                                }
+                                else
+                                {
+                                        has=true;
+                                        break;
+                                }
+                        }
+                }
+                if(has)
+                {
+                        int j=0;
+                        pch= strtok(NULL, "|");
+                        while(pch!='\0')
+                        {
+                                lines.push_back(pch);
+                                pch=strtok(NULL, "|");
+                        }
+                        int fd[2];
+                        if(pipe(fd)==-1)
+                                perror("Error in pipe");
+                        pid=fork();
+                        if(pid<0)
+                                perror("fork error");
+                        else if(pid==0)
+                        {
+                                if(dup2(fd[1],1)==-1)
+                                        perror("dup2");
+                                if(-1==close(fd[0]))
+                                        perror("close");
+                                if(-1==execvp(cmdlist[0], cmdlist))
+                                        perror("execvp");
+                                exit(1);
+                        }
+                        else
+                        {
+                                int stdinput=dup(0);
+                                if(stdinput==-1)
+                                        perror("dup");
+                                if(dup2(fd[0],0)==-1)
+                                        perror("dup2");
+                                if(close(fd[1]))
+                                        perror("close");
+                                if(wait(NULL)==-1)
+                                        perror("wait");
+                                while(lines.size()!=0)
+                                {
+                                        for(int i=0; i<500;i++)
+                                                cmdlist[i]=NULL;
+                                        j=0;
+                                        pch=strtok(lines.at(0), " ");
+                                        cmdlist[j]=pch;
+                                        while(pch!=NULL)
+                                        {
+                                                pch=strtok(NULL, " ");
+                                                j++;
+                                                cmdlist[j]=pch;
+                                        }
+                                        lines.erase(lines.begin());
+                                        if(lines.size()==0)
+                                        {
+                                                execute(cmdlist);
+                                        }
+                                        else
+                                        {
+                                                execute2(cmdlist);
+                                        }
+                                }
+                                if(dup2(stdinput,0)==-1)
+                                        perror("stdin restore failed");
+                        }
+                }
+        }
+        return 0;
 }
+
